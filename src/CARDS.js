c3ddc6dd7b6c1f4f237886d51af2a09dd3e0c36f
Mcorduk <om.corduk@gmail.com>
2024-01-04 15:43:09 +0300

    fix: change card data to collectors number in accordance with API docs

---
+++ src/CARDS.js
@@ -0,0 +1,18 @@
+// Cards that will be used for the memory game
+// Customize by finding the card at this url first: https://scryfall.com/
+// Then entering integer after set letters, here it is 211 https://scryfall.com/card/mid/211
+// Use below as examples
+const CARDS = [
+  "211",
+  "231",
+  "157",
+  "246",
+  "205",
+  "181",
+  "187",
+  "190",
+  "104",
+  "7",
+];
+
+export { CARDS };
